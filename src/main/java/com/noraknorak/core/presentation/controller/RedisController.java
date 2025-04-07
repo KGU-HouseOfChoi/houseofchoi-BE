99169327ec774dcd72136d2c9a49e0427151a680
chyun7114 <chyun7114@naver.com>
2025-04-07 16:49:17 +0900

    fix: RequestParam 제거

--- src/main/java/com/noraknorak/core/presentation/controller/RedisController.java
+++ src/main/java/com/noraknorak/core/presentation/controller/RedisController.java
@@ -12,14 +12,17 @@ import org.springframework.web.bind.annotation.*;
 public class RedisController {
     private final RedisTemplate redisTemplate;
 
+    private String key = "hello";
+    private String value = "redis";
+
     @PostMapping("/set")
-    public ResponseEntity<RestResponse<Boolean>> setValue(@RequestParam String key, @RequestParam String value) {
+    public ResponseEntity<RestResponse<Boolean>> setValue() {
         redisTemplate.opsForValue().set(key, value);
         return ResponseEntity.ok(new RestResponse<>(true));
     }
 
     @GetMapping("/get")
-    public ResponseEntity<RestResponse<String>> getValue(@RequestParam String key) {
+    public ResponseEntity<RestResponse<String>> getValue() {
         String value = (String) redisTemplate.opsForValue().getAndDelete(key);
         return value != null
                 ? ResponseEntity.ok(new RestResponse<>(value))
