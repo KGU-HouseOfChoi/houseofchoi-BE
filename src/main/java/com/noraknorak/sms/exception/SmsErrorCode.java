2918d3522b6763151adaf6480601c5e16d872576
chyun7114 <chyun7114@naver.com>
2025-04-07 17:44:39 +0900

    feat: 코드 인증 및 검증 로직 작성

--- src/main/java/com/noraknorak/sms/exception/SmsErrorCode.java
+++ src/main/java/com/noraknorak/sms/exception/SmsErrorCode.java
@@ -1,4 +1,24 @@
 package com.noraknorak.sms.exception;
 
-public enum SmsErrorCode {
+import com.noraknorak.core.config.exception.DomainException;
+import com.noraknorak.core.config.exception.error.BaseErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@RequiredArgsConstructor
+@Getter
+public enum SmsErrorCode implements BaseErrorCode {
+    NOT_EQUAL_CODE(HttpStatus.BAD_REQUEST, "인증번호를 잘못 입력했습니다."),
+    REDIS_SAVE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"레디스 서버에 값을 저장하는데 실패했습니다."),
+    REDIS_FIND_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "레디스 서버에서 값을 찾아오지 못했습니다.");
+
+    private final HttpStatus httpStatus;
+
+    private final String message;
+
+    @Override
+    public Exception toException() {
+        return new DomainException(httpStatus, message);
+    }
 }
