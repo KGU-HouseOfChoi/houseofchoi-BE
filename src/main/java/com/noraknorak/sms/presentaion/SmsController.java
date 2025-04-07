d3d2659ed1c4ddc5e7e7adb5287eada463cc3f16
Lee Wonbin <ah0416@Naver.com>
2025-04-07 15:23:35 +0900

    feat: coolSMS를 사용하여 인증 문자를 보낸다.

--- src/main/java/com/noraknorak/sms/presentaion/SmsController.java
+++ src/main/java/com/noraknorak/sms/presentaion/SmsController.java
@@ -1,4 +1,23 @@
 package com.noraknorak.sms.presentaion;
 
+import com.noraknorak.core.presentation.RestResponse;
+import com.noraknorak.sms.domain.dto.request.SmsRequest;
+import com.noraknorak.sms.service.SmsService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/sms")
+@RequiredArgsConstructor
 public class SmsController {
+
+    private final SmsService smsService;
+
+    @PostMapping("/send")
+    public ResponseEntity<RestResponse<Boolean>> send(@Valid @RequestBody SmsRequest request) {
+        smsService.sendSms(request.getPhoneNum());
+        return ResponseEntity.ok(new RestResponse<>(true));
+    }
 }
