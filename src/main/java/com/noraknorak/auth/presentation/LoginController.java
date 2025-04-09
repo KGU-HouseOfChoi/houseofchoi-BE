23eddbe12b800db99867f7f2a6a621cca8d4f695
chyun7114 <chyun7114@naver.com>
2025-04-09 19:41:30 +0900

    feat: 쿠키 설정 및 로그아웃 기능 구현

--- src/main/java/com/noraknorak/auth/presentation/LoginController.java
+++ src/main/java/com/noraknorak/auth/presentation/LoginController.java
@@ -3,9 +3,12 @@ package com.noraknorak.auth.presentation;
 import com.noraknorak.auth.application.LoginService;
 import com.noraknorak.auth.dto.request.LoginReqeust;
 import com.noraknorak.auth.dto.response.TokenResponse;
+import com.noraknorak.auth.infrastructure.JwtProperties;
 import com.noraknorak.core.presentation.RestResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,12 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class LoginController implements LoginSwagger{
     private final LoginService loginService;
+    private final JwtProperties jwtProperties;
 
     @Override
     @PostMapping("/login")
     public ResponseEntity<RestResponse<TokenResponse>> login(@Valid @RequestBody LoginReqeust loginReqeust) {
         TokenResponse tokenResponse = loginService.login(loginReqeust);
+        String accessToken = tokenResponse.accessToken();
+        long accessCookieMaxAge = jwtProperties.getAccessExpiration() / 1000;
 
-        return ResponseEntity.ok(new RestResponse<>(tokenResponse));
+        ResponseCookie cookie = ResponseCookie.from("accessToken", accessToken)
+                .path("/")
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .maxAge(accessCookieMaxAge)
+                .build();
+
+        return ResponseEntity
+                .ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(new RestResponse<>(tokenResponse));
     }
 }
---
+++ src/main/java/com/noraknorak/auth/presentation/LogoutController.java
@@ -0,0 +1,31 @@
+package com.noraknorak.auth.presentation;
+
+import com.noraknorak.core.presentation.RestResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class LogoutController implements LogoutSwagger {
+
+    @PostMapping("/logout")
+    public ResponseEntity<RestResponse<Void>> logout() {
+        ResponseCookie cookie = ResponseCookie.from("accessToken", "")
+                .path("/")
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .maxAge(0)
+                .build();
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
+    }
+}
