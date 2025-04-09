abe8bfd038e198243820ee60f333b7a97f4e563b
chyun7114 <chyun7114@naver.com>
2025-04-09 19:41:30 +0900

    feat: 쿠키 설정 및 로그아웃 기능 구현

---
+++ src/main/java/com/noraknorak/auth/presentation/LogoutSwagger.java
@@ -0,0 +1,18 @@
+package com.noraknorak.auth.presentation;
+
+import com.noraknorak.core.config.swagger.ApiErrorCode;
+import com.noraknorak.core.presentation.RestResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+
+@Tag(name = "auth", description = "로그인/회원가입 관련 컨트롤러")
+public interface LogoutSwagger {
+    @Operation(
+            summary = "로그아웃 API",
+            description = "사용자가 로그아웃을 진행합니다.",
+            operationId = "/auth/logout"
+    )
+    @ApiErrorCode({})
+    ResponseEntity<RestResponse<Void>> logout();
+}
