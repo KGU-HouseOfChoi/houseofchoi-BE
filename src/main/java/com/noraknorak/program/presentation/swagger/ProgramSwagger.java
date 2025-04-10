a425742545807a835f955c891c7f7a2bb9d2831b
Lee Wonbin <ah0416@Naver.com>
2025-04-10 16:28:50 +0900

    refactor: Getmapping 수정

--- src/main/java/com/noraknorak/program/presentation/swagger/ProgramSwagger.java
+++ src/main/java/com/noraknorak/program/presentation/swagger/ProgramSwagger.java
@@ -16,20 +16,17 @@ public interface ProgramSwagger {
             summary = "전체 프로그램 조회",
             description = "등록된 모든 프로그램 목록을 조회합니다."
     )
-    @GetMapping
     ResponseEntity<RestResponse<List<ProgramDetailDto>>> getAllPrograms();
 
     @Operation(
             summary = "ID로 프로그램 조회",
             description = "ID를 이용해 특정 프로그램의 상세 정보를 조회합니다."
     )
-    @GetMapping("/{id}")
     ResponseEntity<RestResponse<ProgramDetailDto>> getProgramById(@PathVariable Long id);
 
     @Operation(
             summary = "이름으로 프로그램 검색",
             description = "입력한 이름이 포함된 프로그램들을 검색합니다."
     )
-    @GetMapping("/search")
     ResponseEntity<RestResponse<List<ProgramDetailDto>>> searchProgramsByName(@RequestParam("name") String name);
 }
