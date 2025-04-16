0fd43619809aadecdc7470d1d3f1df516023000e
chyun7114 <chyun7114@naver.com>
2025-04-16 12:26:04 +0900

    feat: JwtAuthenticationFilter 적용

--- src/main/java/com/noraknorak/core/presentation/controller/DatabaseController.java
+++ src/main/java/com/noraknorak/core/presentation/controller/DatabaseController.java
@@ -1,7 +1,9 @@
 package com.noraknorak.core.presentation.controller;
 
+import com.noraknorak.core.presentation.RestResponse;
 import com.noraknorak.core.presentation.swagger.DBTestSwagger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,20 +22,20 @@ public class DatabaseController implements DBTestSwagger {
 
     @Override
     @GetMapping("/db")
-    public String testDatabaseConnection() {
+    public ResponseEntity<RestResponse<String>> testDatabaseConnection() {
         try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement();
              ResultSet resultSet = statement.executeQuery("SELECT NOW()")) {
 
             if (resultSet.next()) {
-                return "DB 연결 성공! 현재 시간: " + resultSet.getString(1);
+                return ResponseEntity.ok(new RestResponse<>("DB 연결 성공!! : " + resultSet.getString(1)));
             } else {
-                return "DB 연결은 되었지만 데이터를 가져오지 못했습니다.";
+                return ResponseEntity.ok(new RestResponse<>("DB 연결은 되었지만 데이터를 가져오지 못했습니다."));
             }
 
         } catch (Exception e) {
             e.printStackTrace();
-            return "DB 연결 실패: " + e.getMessage();
+            return ResponseEntity.ok(new RestResponse<>( "DB 연결 실패: " + e.getMessage()));
         }
     }
 }
