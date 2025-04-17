41e797023e7233ef541e7d1d56e4ecdac7df4ef5
chyun7114 <chyun7114@naver.com>
2025-04-17 19:18:05 +0900

    feat: Cookie Generator를 만든다.

---
+++ src/main/java/com/noraknorak/auth/infrastructure/CookieGenerator.java
@@ -0,0 +1,23 @@
+package com.noraknorak.auth.infrastructure;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CookieGenerator {
+    private final JwtProperties jwtProperties;
+
+    public ResponseCookie generateCookie(String content) {
+        long accessCookieMaxAge = jwtProperties.getAccessExpiration() / 1000; // 쿠키 유효시간 1시간
+
+        return ResponseCookie.from(content)
+                .path("/")
+                .httpOnly(true)
+                .secure(true)
+                .sameSite("None")
+                .maxAge(accessCookieMaxAge)
+                .build();
+    }
+}
