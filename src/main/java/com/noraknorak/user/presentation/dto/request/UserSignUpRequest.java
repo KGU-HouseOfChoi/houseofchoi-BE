8aaf9ff0cee1cab9a8d74ed7587197d113ed8f1e
chyun7114 <chyun7114@naver.com>
2025-04-19 23:40:45 +0900

    feat: UserSignUpRequestTest 코드 작성

--- src/main/java/com/noraknorak/user/presentation/dto/request/UserSignUpRequest.java
+++ src/main/java/com/noraknorak/user/presentation/dto/request/UserSignUpRequest.java
@@ -2,6 +2,7 @@ package com.noraknorak.user.presentation.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 import java.util.Map;
@@ -13,6 +14,9 @@ public record UserSignUpRequest(
     @Schema(name = "사용자 이름", description = "홍길동 -> 이런 이름 3자")
     String name,
 
+    @Pattern(regexp = "^(010|011|016|017|018|019)\\d{7,8}$",
+             message = "전화번호 형식이 잘못되었습니다.")
+    @NotBlank(message = "전화번호를 입력하셔야 합니다.")
     @Schema(name = "전화번호", description = "01012345678 (전화번호 010 포함 '-' 제외")
     String phone,
 
@@ -20,7 +24,8 @@ public record UserSignUpRequest(
     String birth,
 
     @Pattern(regexp = "\\d{6}", message = "인증번호는 6자리 숫자입니다.")
-    @NotBlank(message = "인증번호를 입력해야 합니다.")
+    @NotBlank(message = "인증 코드를 입력해야 합니다.")
+    @Schema(name = "인증 코드", description = "인증번호 정수 6자리 숫자")
     String code
 ){
     public static Map<String, Object> swaggerExample() {
