13600805b3273510da51ae7c30b533437c471a5d
Lee Wonbin <ah0416@Naver.com>
2025-05-09 13:45:38 +0900

    feat: 일정 삭제 기능을 작성한다

--- src/main/java/com/noraknorak/schedule/domain/repository/ScheduleRepository.java
+++ src/main/java/com/noraknorak/schedule/domain/repository/ScheduleRepository.java
@@ -4,9 +4,11 @@ import com.noraknorak.schedule.domain.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     List<Schedule> findByUserId(Long userId);
     boolean existsByUserIdAndProgramId(Long userId, Long programId);
+    Optional<Schedule> findByUserIdAndProgramId(Long userId, Long programId);
 
 }
