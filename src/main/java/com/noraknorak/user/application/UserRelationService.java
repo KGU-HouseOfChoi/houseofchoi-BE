cedcb9e5f4142b1e486107903668fbc076a49a0a
Lee Wonbin <ah0416@Naver.com>
2025-05-12 19:56:06 +0900

    feat: 관계 설정할 때 성향 태그를 가져오는 코드 작성

--- src/main/java/com/noraknorak/user/application/UserRelationService.java
+++ src/main/java/com/noraknorak/user/application/UserRelationService.java
@@ -2,6 +2,7 @@ package com.noraknorak.user.application;
 
 import com.noraknorak.core.config.exception.DomainException;
 import com.noraknorak.core.infrastructure.security.CustomUserDetails;
+import com.noraknorak.user.domain.Personality;
 import com.noraknorak.user.domain.Role;
 import com.noraknorak.user.domain.User;
 import com.noraknorak.user.domain.repository.UserRepository;
@@ -22,27 +23,50 @@ public class UserRelationService {
                 .orElseThrow(UserErrorCode.NOT_EQUAL_USER_CODE::toException);
     }
 
-    // 부모/자식 관계 설정
+    // 부모/자식 관계 설정 + 자식의 personality 태그를 부모로부터 복사
     @Transactional
-    public void verifyRelatedUser(CustomUserDetails customUserDetails, String role, Long relatedUserId){
+    public void verifyRelatedUser(CustomUserDetails customUserDetails, String role, Long relatedUserId) {
         Long userId = customUserDetails.getId();
 
         if (userId.equals(relatedUserId)) {
             throw UserErrorCode.SELF_RELATION_NOT_ALLOWED.toException();
         }
 
-        try{
-            if(Role.SENIOR.getName().equals(role)){
+        try {
+            User me = userRepository.findById(userId)
+                    .orElseThrow(UserErrorCode.USER_NOT_FOUND::toException);
+
+            User relatedUser = userRepository.findById(relatedUserId)
+                    .orElseThrow(UserErrorCode.USER_NOT_FOUND::toException);
+
+            if (Role.SENIOR.getName().equals(role)) {
                 userRepository.updateUserRoleAndRelatedUser(userId, Role.GUARDIAN, relatedUserId);
-            }else if(Role.GUARDIAN.getName().equals(role)){
+
+            } else if (Role.GUARDIAN.getName().equals(role)) {
                 userRepository.updateUserRoleAndRelatedUser(userId, Role.SENIOR, relatedUserId);
-            }else{
+
+                if (relatedUser.getPersonality() != null) {
+                    me.setPersonality(null);
+
+                    Personality copied = Personality.builder()
+                            .tag(relatedUser.getPersonality().getTag())
+                            .ei(relatedUser.getPersonality().getEi())
+                            .sn(relatedUser.getPersonality().getSn())
+                            .tf(relatedUser.getPersonality().getTf())
+                            .pj(relatedUser.getPersonality().getPj())
+                            .user(me)
+                            .build();
+
+                    me.setPersonality(copied);
+                }
+
+            } else {
                 throw UserErrorCode.INVALID_ROLE_ERROR.toException();
             }
+
         } catch (DomainException e) {
             throw e;
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             throw UserErrorCode.INTERNAL_SERVER_ERROR.toException();
         }
     }
