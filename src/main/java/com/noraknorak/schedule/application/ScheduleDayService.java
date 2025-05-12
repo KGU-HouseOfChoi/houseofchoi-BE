219d2539fb641fff2018ac016895b504b11caed5
Lee Wonbin <ah0416@Naver.com>
2025-05-12 22:07:47 +0900

    feat: 요일을 통한 일정 검색도 관계 설정한 유저의 일정까지 반환

--- src/main/java/com/noraknorak/schedule/application/ScheduleDayService.java
+++ src/main/java/com/noraknorak/schedule/application/ScheduleDayService.java
@@ -6,38 +6,55 @@ import com.noraknorak.schedule.domain.Schedule;
 import com.noraknorak.schedule.domain.repository.ScheduleRepository;
 import com.noraknorak.schedule.exception.ScheduleErrorCode;
 import com.noraknorak.schedule.presentation.dto.response.ScheduleDayDto;
+import com.noraknorak.user.domain.User;
+import com.noraknorak.user.domain.repository.UserRepository;
+import com.noraknorak.user.exception.UserErrorCode;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleDayService {
 
     private final ScheduleRepository scheduleRepository;
+    private final UserRepository userRepository;
     private final ImageUrlGenerator imageUrlGenerator;
 
     @Transactional
     public List<ScheduleDayDto> getSchedulesByDay(Long userId, String dayKorean) {
-        List<Schedule> schedules = scheduleRepository.findByUserId(userId);
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> UserErrorCode.USER_NOT_FOUND.toException());
 
-        if(schedules == null || schedules.isEmpty()) {
+        Set<Long> userIdsToSearch = new HashSet<>();
+        userIdsToSearch.add(user.getId());
+        if (user.getRelatedUser() != null) {
+            userIdsToSearch.add(user.getRelatedUser());
+        }
+
+        List<Schedule> schedules = scheduleRepository.findByUserIdIn(new ArrayList<>(userIdsToSearch));
+        if (schedules.isEmpty()) {
             throw ScheduleErrorCode.SCHEDULE_NOT_FOUND.toException();
         }
 
-        List<ScheduleDayDto> filtered = filteringScheduleByDays(
-                schedules,
-                dayKorean
-        );
-
+        List<ScheduleDayDto> filtered = filteringScheduleByDays(schedules, dayKorean);
         if (filtered.isEmpty()) {
             throw ScheduleErrorCode.NO_PROGRAM_FOR_DAY.toException();
         }
 
-        return filtered;
+        return filtered.stream()
+                .collect(Collectors.toMap(
+                        ScheduleDayDto::programId,
+                        Function.identity(),
+                        (d1, d2) -> d1
+                ))
+                .values()
+                .stream()
+                .toList();
     }
 
     private List<ScheduleDayDto> filteringScheduleByDays(List<Schedule> schedules, String dayKorean) {
